137413a89fbd878301f4b1e71f2fbcc42d496370
emarkees <rphillipse@gmail.com>
2023-10-19 15:36:55 -0600

    Add greetings files

--- src/Components/Greetings.jsx
+++ src/Components/Greetings.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import { useSelector } from 'react-redux'
 
- const Greetings = () => {
+const Greeting = () => {
+const {randomGreeting, isLoading} = useSelector((store) => store.greetings)  
+
+
   return (
-    <>
-    </>
-  );
- };
+    <div>
+     {isLoading ? <h1>Loading...</h1> : <h1>{randomGreeting.greetings}</h1>}
+    </div>
+  )
+}
 
- export default Greetings;
+export default Greeting
